672a93712bf2a1d3df6ca8a19b507ca6887a4c92
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-02 17:38:00 -0400

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import equals from './images/equals.jpg'
 
 export function App() {
+  const [currencyResults, setCurrencyResults] = useState({ rates: [] })
+  const [amount, setAmount] = useState(1)
+
+  useEffect(async () => {
+    const response = await axios.get(
+      `https://api.exchangeratesapi.io/v1/latest`
+    )
+    console.log('yes')
+    setCurrencyResults(response.data)
+  }, [])
   return (
     <body>
       <header>
-        <h1>How Badly is My Currency Devaluing?</h1>
+        <h1>Currency calculator</h1>
       </header>
       <main>
         <form>
           <input type="text" placeholder="0.00" />
         </form>
         <button>Show me the rates!</button>
+        <ul>
+          <li>$inputted number</li>
+          <li>
+            <img src={equals} height="50" alt="eqals sign" />
+          </li>
+          <li>rates</li>
+        </ul>
       </main>
     </body>
   )
