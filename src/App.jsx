3f2228cfed44c9cd0b48dd597e41ad52d1f40788
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-02 14:26:32 -0400

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <body>
+      <header>
+        <h1>How Badly is My Currency Devaluing?</h1>
+      </header>
+      <main>
+        <form>
+          <input type="text" placeholder="0.00" />
+        </form>
+        <button>Show me the rates!</button>
+      </main>
+    </body>
+  )
 }
